7fa0d97f3e4d019b62c6b9275063e7952c2eee5a
ranggaju <ranggajzp@gmail.com>
2025-11-05 16:30:40 +0700

    feat: components

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full h-16 bg-black rounded-lg flex items-center">
+      <div className="m-2 p-2 w-16 bg-white rounded-[inherit]">AA</div>
+    </nav>
+  );
+}
---
+++ src/providers/pageProvider.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/navbar";
+
+interface PageProviderProps {
+  children: React.ReactNode;
+}
+
+export default function PageProvider({ children }: PageProviderProps) {
+  return (
+    <>
+      <Navbar />
+      {children};
+    </>
+  );
+}
